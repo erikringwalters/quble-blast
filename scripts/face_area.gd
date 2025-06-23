5a2290d47dcf0d7c7232d4743f87cd5b02b2c5fd
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-06-22 18:50:54 -0700

    add quble on click

--- scripts/face_area.gd
+++ scripts/face_area.gd
@@ -1,12 +1,20 @@
 extends Area3D
 
+signal clicked
+
+@onready var parent: Node3D = get_parent()
+@onready var quble_manager: Node3D = get_parent().get_parent()
 @onready var mesh: MeshInstance3D = %Mesh
+@onready var destination: Node3D = %Destination
 
 func _ready() -> void:
 	connect("input_event", Callable(self, "_on_input_event"))
+	connect("clicked", Callable(quble_manager, "_on_clicked"))
 
-func _on_input_event(_camera: Camera3D, _event: InputEvent, touch_position: Vector3, _normal: Vector3, _shape_idx: float) -> void:
+func _on_input_event(_camera: Camera3D, event: InputEvent, touch_position: Vector3, _normal: Vector3, _shape_idx: float) -> void:
 	handle_input(touch_position)
+	if event.is_action_pressed("left_click"):
+		clicked.emit(destination.global_position)
 
 func handle_input(_touch_position: Vector3) -> void:
 	# print("touch input at: ", touch_position)
---
+++ scripts/quble_manager.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var quble_scene: PackedScene
+
+var quble_width: float = 1.6
+var quble_offset: float = quble_width
+var quble_axis_count: int = 5
+
+func _ready() -> void:
+	spawn_qubles(quble_axis_count)
+	pass
+
+func spawn_qubles(count: int) -> void:
+	for i in count:
+		for j in count:
+			for k in count:
+				var quble: RigidBody3D = quble_scene.instantiate()
+				add_child(quble)
+				var position_offset: float = (quble_width * count * 0.5) - (quble_width * 0.5)
+				quble.position = Vector3(
+					i * quble_offset - position_offset, \
+					j * quble_offset - position_offset, \
+					k * quble_offset - position_offset \
+				)
+
+func _on_clicked(destination: Vector3) -> void:
+	print("clicked")
+	var quble: RigidBody3D = quble_scene.instantiate()
+	add_child(quble)
+	quble.global_position = destination
